aa8738a139e9b7e13539f9afbe6b956a8033befc
zen <zen@talk-talk.net>
2026-05-22 07:00:19 -0700

    filter out non numbers and boolean values

--- count_even/main.py
+++ count_even/main.py
@@ -1,4 +1,4 @@
-def count_even(lst: list[int]) -> int:
+def count_even(lst: list) -> int:
     """
     Return the number of even integers in the list.
 
@@ -8,7 +8,11 @@ def count_even(lst: list[int]) -> int:
     even_count: int = 0
 
     for item in lst:
-        if item % 2 == 0:
+        if (
+            isinstance(item, (int, float))
+            and not isinstance(item, bool)
+            and item % 2 == 0
+        ):
             even_count += 1
 
     return even_count
@@ -17,3 +21,5 @@ def count_even(lst: list[int]) -> int:
 if __name__ == "__main__":
     print(count_even([2, 1, 2, 3, 4]))  # Expected: 3
     print(count_even([1, 2, 3, 4, 5, 6, 7, 8]))  # Expected: 4
+    print(count_even([1, 2, "b", 4, "x", 6, 7, 8]))  # Expected: 4
+    print(count_even([2, False, 2, 3, 4]))  # Expected: 3
