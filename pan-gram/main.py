d105b18c441e627f829bc93716d76f153af530ed
zen <zen@talk-talk.net>
2026-06-19 10:43:56 -0700

    pan-gram

---
+++ pan-gram/main.py
@@ -0,0 +1,26 @@
+"""
+A pan-gram is a sentence where every letter of the English alphabet appears at least once.
+
+Given a string sentence containing only lowercase English letters,
+return true if sentence is a pangram, or false otherwise.
+
+sentence = "thequickbrownfoxjumpsoverthelazydog"
+True
+
+sentence = "leetcode"
+False
+
+https://leetcode.com/problems/check-if-the-sentence-is-pangram/description/
+"""
+
+
+def checkIfPangram(sentence: str) -> bool:
+    # return set(sentence) == set("abcdefghijklmnopqrstuvwxyz")
+
+    return len(set(sentence)) == 26
+
+
+if __name__ == "__main__":
+    sentence = "thequickbrownfoxjumpsoverthelazydog"
+    # sentence = "coachable"
+    print(checkIfPangram(sentence))
